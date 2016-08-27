3d7f255a07dc4d08660c566e2d7f318d487329b6
Yanuar Nurcahyo <yanuar.nurcahyo6661@gmail.com>
2016-08-27 09:48:39 +0700

    PDF Final Paper

--- python/books.py
+++ python/books.py
@@ -2,5 +2,5 @@
 # === PYTHON PROGRAMMING ===
 
 import requests, json
-r = requests.get( 'http://localhost:2000/books', auth=('user', 'pass'))
+r = requests.get( 'http://localhost:2000/journals', auth=('user', 'pass'))
 print(json.dumps( r.json(), sort_keys=True,indent=4, separators=(',', ': ')))
